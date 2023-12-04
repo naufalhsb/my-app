719f639d9022bd817ef8b5dfd0fe922edbb4f463
naufal <ahmadnaufalhsb13@gmail.com>
2023-12-04 11:01:24 +0700

    dua

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,9 @@ import { Box, Button, FormControl, InputLabel, Input, FormHelperText } from "@mu
 export default function Home() {
   return (
     <Box>
+      <h1 className="mt-20">Login</h1>
       <FormControl>
+        <InputLabel htmlFor="my-input">Email address</InputLabel>
         <InputLabel htmlFor="my-input">Email address</InputLabel>
         <Input id="my-input" aria-describedby="my-helper-text" />
         <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
